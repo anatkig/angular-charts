b67ec982f0b8a929833c3df1c65a9cfa81c1d9a0
anatkig <keeggooggle@gmail.com>
2023-11-02 16:01:27 +0100

    inserted ag grid

--- projectChart/src/app/app.component.ts
+++ projectChart/src/app/app.component.ts
@@ -6,6 +6,10 @@ import HC_Data from 'highcharts/modules/export-data';
 import Accessbility from 'highcharts/modules/accessibility';
 import { Component, VERSION, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 HC_exporting(Highcharts);
 HC_Data(Highcharts);
@@ -19,6 +23,10 @@ Accessbility(Highcharts);
 })
 export class AppComponent {
   @ViewChild('lineChart', { static: false }) lineChart: any;
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+  constructor(private http: HttpClient) {}
+
   Highcharts: typeof Highcharts = Highcharts;
   updateFlag = false;
   data = [1, 2, 3, 4];
@@ -72,4 +80,37 @@ export class AppComponent {
       categories: ['10222023', '11222023'],
     },
   };
+
+  
+  // Each Column Definition results in one Column.
+  public columnDefs: ColDef[] = [
+    { field: 'make'},
+    { field: 'model'},
+    { field: 'price' }
+  ];
+
+  // DefaultColDef sets props common to all Columns
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+  
+  // Data that gets displayed in the grid
+  public rowData$!: Observable<any[]>;
+
+  // Example load data from server
+  onGridReady(params: GridReadyEvent) {
+    this.rowData$ = this.http
+      .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+  }
+
+  // Example of consuming Grid Event
+  onCellClicked( e: CellClickedEvent): void {
+    console.log('cellClicked', e);
+  }
+
+  // Example using Grid's API
+  clearSelection(): void {
+    this.agGrid.api.deselectAll();
+  }
 }
--- projectChart/src/app/app.module.ts
+++ projectChart/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HighchartsChartModule
+    HighchartsChartModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
